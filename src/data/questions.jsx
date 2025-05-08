459367d2fadd5b6a3702db9ab5c1883d3bbea6e9
Vivek Pradhan <vivekpradhan736@gmail.com>
2025-05-08 11:27:18 +0530

    Add src/data/questions.jsx from Buildit.ai

---
+++ src/data/questions.jsx
@@ -0,0 +1,46 @@
+export const questions = [
+    {
+      id: 1,
+      questionText: 'Who was the first President of the United States?',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/b/b6/Gilbert_Stuart_Williamstown_Portrait_of_George_Washington.jpg',
+      answerOptions: [
+        { id: '1', answerText: 'Thomas Jefferson', isCorrect: false },
+        { id: '2', answerText: 'George Washington', isCorrect: true },
+        { id: '3', answerText: 'John Adams', isCorrect: false },
+        { id: '4', answerText: 'James Madison', isCorrect: false },
+      ],
+    },
+    {
+      id: 2,
+      questionText: 'In which year did World War II end?',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/4/4f/Potsdam_Conference_seated.jpg',
+      answerOptions: [
+        { id: '1', answerText: '1943', isCorrect: false },
+        { id: '2', answerText: '1944', isCorrect: false },
+        { id: '3', answerText: '1945', isCorrect: true },
+        { id: '4', answerText: '1946', isCorrect: false },
+      ],
+    },
+    {
+      id: 3,
+      questionText: 'Who painted the Mona Lisa?',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/e/ec/Mona_Lisa%2C_by_Leonardo_da_Vinci%2C_from_C2RMF_retouched.jpg',
+      answerOptions: [
+        { id: '1', answerText: 'Michelangelo', isCorrect: false },
+        { id: '2', answerText: 'Leonardo da Vinci', isCorrect: true },
+        { id: '3', answerText: 'Raphael', isCorrect: false },
+        { id: '4', answerText: 'Donatello', isCorrect: false },
+      ],
+    },
+    {
+      id: 4,
+      questionText: 'Which ancient civilization built Machu Picchu?',
+      image: 'https://upload.wikimedia.org/wikipedia/commons/e/eb/Machu_Picchu%2C_Peru.jpg',
+      answerOptions: [
+        { id: '1', answerText: 'Roman', isCorrect: false },
+        { id: '2', answerText: 'Greek', isCorrect: false },
+        { id: '3', answerText: 'Inca', isCorrect: true },
+        { id: '4', answerText: 'Egyptian', isCorrect: false },
+      ],
+    },
+  ];
